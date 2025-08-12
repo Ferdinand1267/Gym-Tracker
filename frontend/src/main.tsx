56a240c28c544032bea445e6e629dc32c43541e3
Ferdinand1267 <ferdinand.changb2026@stu.tes.tp.edu.tw>
2025-08-12 17:28:12 +0800

    start login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
