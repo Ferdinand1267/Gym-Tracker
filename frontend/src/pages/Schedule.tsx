b3fb6a9802f009bf38517b7ffe8d8e88e23c615f
Ferdinand1267 <ferdinand.changb2026@stu.tes.tp.edu.tw>
2025-09-01 01:20:07 +0800

    schedule page

--- frontend/src/pages/Schedule.tsx
+++ frontend/src/pages/Schedule.tsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import './Schedule.css';
 import NavBar from '../NavBar'
+import { Info, DateTime } from "luxon"
+
+type ScheduledWorkout = {
+    date: string;
+    workoutType: string;
+    time: string;
+}
+
 
 function Schedule() {
+    const date = new Date()
+    const todayDate = date.getDate()
+    const todayDay = date.getDay()
+    const dateSunday = todayDate - todayDay
+
     return (
     <div>
         <img src="/logo.png" alt="logo" className="logo" width="100"></img>
