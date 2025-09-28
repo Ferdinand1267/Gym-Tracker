53864646bd7df325eb1da032e068be3b4c1bcc44
Ferdinand1267 <ferdinand.changb2026@stu.tes.tp.edu.tw>
2025-09-28 18:04:47 +0800

    edits

---
+++ backend/src/main/java/com/gymtracker/backend/service/UserService.java
@@ -0,0 +1,7 @@
+package com.gymtracker.backend.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+import org.springframework.stereotype.Service; //this imports @Service to indicate that it is a spring service that contains logic
+public class UserService {
+    
+}
